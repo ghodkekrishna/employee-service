2b6076c137dd081825cfcedc786e185929dd7b2f
Krishna Ghodke <krushnaghodke90@gmail.com>
2024-03-05 10:37:28 -0800

    Microservices Communication using WebClient

--- src/main/java/net/codefusionhub/employeeservice/service/Impl/EmployeeServiceImpl.java
+++ src/main/java/net/codefusionhub/employeeservice/service/Impl/EmployeeServiceImpl.java
@@ -10,6 +10,7 @@ import net.codefusionhub.employeeservice.service.EmployeeService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Optional;
 
@@ -19,7 +20,8 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
 
-    private RestTemplate restTemplate;
+//    private RestTemplate restTemplate;
+    private WebClient webClient;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -46,10 +48,16 @@ public class EmployeeServiceImpl implements EmployeeService {
         Optional<Employee> employeeObj = employeeRepository.findById(employeeId);
         Employee employee = employeeObj.get();
 
-        ResponseEntity<DepartmentDto>  responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/"
-                        +employee.getDepartmentCode(), DepartmentDto.class);
+        DepartmentDto departmentDto = webClient.get()
+                .uri("http://localhost:8080/api/departments/" +employee.getDepartmentCode())
+                .retrieve()
+                .bodyToMono(DepartmentDto.class)
+                .block();
 
-        DepartmentDto departmentDto = responseEntity.getBody();
+//        ResponseEntity<DepartmentDto>  responseEntity = restTemplate.getForEntity("http://localhost:8080/api/departments/"
+//                        +employee.getDepartmentCode(), DepartmentDto.class);
+//
+//        DepartmentDto departmentDto = responseEntity.getBody();
 
         EmployeeDto employeeDto =  new EmployeeDto(
                 employee.getId(),
