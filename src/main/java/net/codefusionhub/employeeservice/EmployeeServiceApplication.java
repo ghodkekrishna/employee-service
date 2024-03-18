9dcc7e0d4fc2fce6545a8daac78d7db4a52808f4
Krishna Ghodke <krushnaghodke90@gmail.com>
2024-03-17 22:22:54 -0700

    Employee-Service - REST API Documentation

--- src/main/java/net/codefusionhub/employeeservice/EmployeeServiceApplication.java
+++ src/main/java/net/codefusionhub/employeeservice/EmployeeServiceApplication.java
@@ -1,5 +1,10 @@
 package net.codefusionhub.employeeservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -9,6 +14,25 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableFeignClients
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Employee Service REST APIs",
+                description = "Employee Service REST APIs Documentation",
+                version = "v1.0",
+                contact = @Contact(
+                        name = "Krishna",
+                        email = "krushnaghodke90@gmail.com",
+                        url = "https://krishnaghodke.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "Employee Service Doc.",
+                url = "https://krishnaghodke.com"
+        )
+)
 public class EmployeeServiceApplication {
 
     @Bean
--- src/main/java/net/codefusionhub/employeeservice/controller/EmployeeController.java
+++ src/main/java/net/codefusionhub/employeeservice/controller/EmployeeController.java
@@ -1,5 +1,8 @@
 package net.codefusionhub.employeeservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import net.codefusionhub.employeeservice.dto.APIResponseDto;
 import net.codefusionhub.employeeservice.dto.EmployeeDto;
@@ -11,16 +14,36 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/employees")
+@Tag(
+        name = "Employee Service - Employee Controller",
+        description = "Employee Controller Exposes REST APIs for Employee-Service"
+)
 public class EmployeeController {
     private EmployeeService employeeService;
     
     @PostMapping
+    @Operation(
+            summary = "Save Employee REST API",
+            description = "Save employee REST API is used to save employee object in database."
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
     public ResponseEntity<EmployeeDto> saveEmployee(@RequestBody EmployeeDto employeeDto){
         EmployeeDto savedEmployee = employeeService.saveEmployee(employeeDto);
         return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
     }
 
     @GetMapping("{employee-id}")
+    @Operation(
+            summary = "Get Employee REST API",
+            description = "Get employee REST API is used to get employee details in database."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
     public ResponseEntity<APIResponseDto> getEmployee(@PathVariable("employee-id") Long employeeId){
         APIResponseDto employeeDto = employeeService.getEmployee(employeeId);
 
